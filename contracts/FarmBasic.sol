3851db07b806b470132cc707be667640b869a890
Julien Pipard <j.pipard@useradgents.com>
2021-08-09 13:32:37 +0200

    earn WIP

--- contracts/FarmBasic.sol
+++ contracts/FarmBasic.sol
@@ -9,11 +9,44 @@ import "./libs/IPancakeswapFarm.sol";
 
 contract FarmBasic is ERC20, Ownable {
 
-    IERC20 private farmToken; // can be CAKE or liquidity such as BUSD-USDT
+    IERC20 private farmToken; // can be a single asset or a liquidity such as BUSD-USDT
 
     uint256  private _farmPid;
     bool private isActive;
-    address private _farmContractAddress;
+    address private farmContractAddress;
+    address public earnedAddress; // CAKE or whatever
+
+    address public token0Address;
+    address public token1Address;
+    address public uniRouterAddress; // uniswap, pancakeswap etc
+
+    uint256 public controllerFee = 0; // 70;
+    uint256 public constant controllerFeeMax = 10000; // 100 = 1%
+    uint256 public constant controllerFeeUL = 300;
+
+    uint256 public entranceFeeFactor = 9990; // < 0.1% entrance fee - goes to pool + prevents front-running
+    uint256 public constant entranceFeeFactorMax = 10000;
+    uint256 public constant entranceFeeFactorLL = 9950; // 0.5% is the max entrance fee settable. LL = lowerlimit
+
+    uint256 public withdrawFeeFactor = 10000; // 0.1% withdraw fee - goes to pool
+    uint256 public constant withdrawFeeFactorMax = 10000;
+    uint256 public constant withdrawFeeFactorLL = 9950; // 0.5% is the max entrance fee settable. LL = lowerlimit
+
+    uint256 public slippageFactor = 950; // 5% default slippage tolerance
+    uint256 public constant slippageFactorUL = 995;
+
+    address[] public earnedToToken0Path;
+    address[] public earnedToToken1Path;
+
+    event SetSettings(
+        uint256 _entranceFeeFactor,
+        uint256 _withdrawFeeFactor,
+        uint256 _controllerFee,
+        uint256 _buyBackRate,
+        uint256 _slippageFactor
+    );
+    event SetFarmContractAddress(address _farmContractAddress);
+
 
     modifier onlyActive() {
         assert(isActive == true);
@@ -21,16 +54,16 @@ contract FarmBasic is ERC20, Ownable {
     }
 
     constructor(
-        address farmTokenAddress, 
-        address farmContractAddress, // address of farm, eg, PCS, Thugs etc.
-        uint256 farmPid, // pid of pool in farmContractAddress
+        address _farmTokenAddress, 
+        address _farmContractAddress, // address of farm, eg, PCS, Thugs etc.
+        uint256 _farmPid, // pid of pool in farmContractAddress
         string memory name, 
         string memory symbol
     ) ERC20(name, symbol) Ownable() {
         isActive = false;
-        _farmPid = farmPid;
-        _farmContractAddress = farmContractAddress;
-        farmToken = ERC20(farmTokenAddress);
+        farmPid = _farmPid;
+        farmContractAddress = _farmContractAddress;
+        farmToken = ERC20(_farmTokenAddress);
     }
 
     function depositFarmTokens(uint amount) public onlyActive {
@@ -52,13 +85,24 @@ contract FarmBasic is ERC20, Ownable {
 
     function farm() public {
         uint256 wantAmt = IERC20(farmToken).balanceOf(address(this));
-        IERC20(farmToken).approve(_farmContractAddress, wantAmt);
+        IERC20(farmToken).approve(farmContractAddress, wantAmt);
 
-        IPancakeswapFarm(_farmContractAddress).deposit(_farmPid, wantAmt);
+        IPancakeswapFarm(farmContractAddress).deposit(farmPid, wantAmt);
+    }
+
+    function earn() public onlyActive {
+        // Harvest farm tokens
+        farm();
+
+        // Converts farm tokens into want tokens
+        uint256 earnedAmt = IERC20(earnedAddress).balanceOf(address(this));
+
+        IERC20(earnedAddress).approve(uniRouterAddress, 0);
+        IERC20(earnedAddress).increaseAllowance(uniRouterAddress, earnedAmt);
     }
 
     function unfarm(uint256 wantAmt) public {
-        IPancakeswapFarm(_farmContractAddress).withdraw(_farmPid, wantAmt);
+        IPancakeswapFarm(farmContractAddress).withdraw(farmPid, wantAmt);
     }
 
     function redeemRewards() public onlyActive {
@@ -71,5 +115,58 @@ contract FarmBasic is ERC20, Ownable {
 
     function isActivated() public view returns(bool) {
         return isActive;
+    }
+
+    function setUniRouterAddress(address _farmContractAddress) public virtual onlyOwner{
+        farmContractAddress = _farmContractAddress;
+        emit SetFarmContractAddress(_farmContractAddress);
+    }
+
+    function setSettings(
+        uint256 _entranceFeeFactor,
+        uint256 _withdrawFeeFactor,
+        uint256 _controllerFee,
+        uint256 _buyBackRate,
+        uint256 _slippageFactor
+    ) public virtual onlyOwner {
+        require(
+            _entranceFeeFactor >= entranceFeeFactorLL,
+            "_entranceFeeFactor too low"
+        );
+        require(
+            _entranceFeeFactor <= entranceFeeFactorMax,
+            "_entranceFeeFactor too high"
+        );
+        entranceFeeFactor = _entranceFeeFactor;
+
+        require(
+            _withdrawFeeFactor >= withdrawFeeFactorLL,
+            "_withdrawFeeFactor too low"
+        );
+        require(
+            _withdrawFeeFactor <= withdrawFeeFactorMax,
+            "_withdrawFeeFactor too high"
+        );
+        withdrawFeeFactor = _withdrawFeeFactor;
+
+        require(_controllerFee <= controllerFeeUL, "_controllerFee too high");
+        controllerFee = _controllerFee;
+
+        require(_buyBackRate <= buyBackRateUL, "_buyBackRate too high");
+        buyBackRate = _buyBackRate;
+
+        require(
+            _slippageFactor <= slippageFactorUL,
+            "_slippageFactor too high"
+        );
+        slippageFactor = _slippageFactor;
+
+        emit SetSettings(
+            _entranceFeeFactor,
+            _withdrawFeeFactor,
+            _controllerFee,
+            _buyBackRate,
+            _slippageFactor
+        );
     }
 }
