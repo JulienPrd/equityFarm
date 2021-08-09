d612e9181223c93cce9c23fd6c2a95a7203395c7
Julien PIPARD <j.pipard@useradgents.com>
2021-08-09 21:27:16 +0200

    earn

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -6,6 +6,11 @@ var prefix = "equity";
 // pid: 423
 // contract: 0x73feaa1ee314f8c655e354234017be2193c9e24e
 // liquidity: 0xec6557348085aa57c72514d67070dc863c0a5a8c
+// token1 USDC: 0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d 
+// token2 BUSD: 0x55d398326f99059ff775485246999027b3197955 
+// PCS Router: 0x10ed43c718714eb63d5aa57b78b54704e256024e
+
+// balance WEI : 968417212915858001
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(MetaCoin, "MetaCoin", "META").then(function() {
